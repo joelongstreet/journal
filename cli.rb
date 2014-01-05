8cbee38c8bb47bc681032a47bf62c89b8a34c4c0
joelongstreet <joelongstreet@gmail.com>
2014-01-05 14:51:19 -0600

    breaking up modes into different files

--- cli.rb
+++ cli.rb
@@ -8,19 +8,8 @@ mongo_client = Mongo::MongoClient.from_uri(ENV['JOURNAL_MONGO_URI'])
 db = mongo_client.db()
 entries = db.collection('entries')
 
-input_entry = ask "<%= color('What have you just learned? ', ON_BLACK ) %> "
-input_tags  = ask "<%= color('Tags? ', ON_BLACK) %> "
-
-tags = []
-input_tags.split(',').each do |tag|
-  tags.push tag.strip
+if ARGV[0] == 'read'
+  require_relative 'lib/read.rb'
+else
+  require_relative 'lib/write.rb'
 end
-
-journal_entry = {
-  'entry' => input_entry,
-  'tags'  => tags,
-  'time'  => Time.now
-}
-
-say "\n ... saved"
-entries.insert(journal_entry)
---
+++ lib/write.rb
@@ -0,0 +1,16 @@
+input_entry = ask "<%= color('What have you just learned? ', ON_BLACK ) %> "
+input_tags  = ask "<%= color('Tags? ', ON_BLACK) %> "
+
+tags = []
+input_tags.split(',').each do |tag|
+  tags.push tag.strip
+end
+
+journal_entry = {
+  'entry' => input_entry,
+  'tags'  => tags,
+  'time'  => Time.now
+}
+
+say "\n ... saved"
+entries.insert(journal_entry)
