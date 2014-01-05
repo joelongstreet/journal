6643c2cee4b6438e05b69ea6f6046c9e4c8235aa
joelongstreet <joelongstreet@gmail.com>
2014-01-05 13:07:43 -0600

    add color, clean up code

--- cli.rb
+++ cli.rb
@@ -8,9 +8,8 @@ mongo_client = Mongo::MongoClient.from_uri(ENV['JOURNAL_MONGO_URI'])
 db = mongo_client.db()
 entries = db.collection('entries')
 
-
-input_entry = ask "What have you just learned?"
-input_tags  = ask "Tags?"
+input_entry = ask "<%= color('What have you just learned? ', ON_BLACK ) %> "
+input_tags  = ask "<%= color('Tags? ', ON_BLACK) %> "
 
 tags = []
 input_tags.split(',').each do |tag|
@@ -18,9 +17,10 @@ input_tags.split(',').each do |tag|
 end
 
 journal_entry = {
-  :entry  => input_entry,
-  :tags   => tags,
-  :time   => Time.now
+  'entry' => input_entry,
+  'tags'  => tags,
+  'time'  => Time.now
 }
 
+say "\n ... saved"
 entries.insert(journal_entry)
