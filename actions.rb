96c335acc0923985e653ae7a9979839fdfbabab5
joelongstreet <joelongstreet@gmail.com>
2014-01-05 15:45:30 -0600

    printing out read data, renaming some stuff in write

--- actions.rb
+++ actions.rb
@@ -1,9 +1,12 @@
 module Actions
-  def self.read(entries)
-    puts 'read'
+  def self.read(records)
+    records.find().each do |record|
+      time = record['time'].strftime("%A, %B %d %H:%M")
+      say "<%= color('#{time}', ON_BLACK) %> #{record['entry']}"
+    end
   end
 
-  def self.write(entries)
+  def self.write(records)
     input_entry = ask "<%= color('What have you just learned? ', ON_BLACK ) %> "
     input_tags  = ask "<%= color('Tags? ', ON_BLACK) %> "
 
@@ -12,13 +15,13 @@ module Actions
       tags.push tag.strip
     end
 
-    journal_entry = {
+    new_record = {
       'entry' => input_entry,
       'tags'  => tags,
       'time'  => Time.now
     }
 
     say "\n ... saved"
-    entries.insert(journal_entry)
+    records.insert(new_record)
   end
 end
