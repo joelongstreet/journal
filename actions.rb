a73e1fd0f02d6787c91b307261aab2c19b108ca2
joelongstreet <joelongstreet@gmail.com>
2014-01-06 07:19:33 -0600

    make it look a little bit prettier

--- actions.rb
+++ actions.rb
@@ -7,19 +7,19 @@ module Actions
 
   def self.search(collection, term)
     msg = "searching your journal for entries containing "
-    say "<%= color('#{msg}', ON_BLACK) %> <%= color('#{term}', BOLD) %>\n\n"
+    say "<%= color('#{msg}', REVERSE) %> <%= color('#{term}', BOLD) %>\n\n"
     print_records collection.find({ "entry" => Regexp.new(term) }).sort('time')
   end
 
   def self.tagged(collection, tag)
     msg = "searching your journal for entries tagged "
-    say "<%= color('#{msg}', ON_BLACK) %> <%= color('#{tag}', BOLD) %>\n\n"
+    say "<%= color('#{msg}', REVERSE) %> <%= color('#{tag}', BOLD) %>\n\n"
     print_records collection.find({ "tags" => tag }).sort('time')
   end
 
   def self.write(collection)
-    input_entry = ask "<%= color('What have you just learned? ', ON_BLACK ) %> "
-    input_tags  = ask "<%= color('Tags? ', ON_BLACK) %> "
+    input_entry = ask "<%= color('What have you just learned? ', REVERSE ) %> "
+    input_tags  = ask "<%= color('Tags? ', REVERSE) %> "
 
     tags = []
     input_tags.split(',').each do |tag|
