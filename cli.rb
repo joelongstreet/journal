5d4b145368bac1c022eb6b771b7a6033da10750a
joelongstreet <joelongstreet@gmail.com>
2014-01-05 20:53:15 -0600

    using trollop to parse command line arguments

--- cli.rb
+++ cli.rb
@@ -2,14 +2,24 @@
 
 require 'highline/import'
 require 'mongo'
+require 'trollop'
 require_relative 'actions.rb'
 
-say "Preparing journal... \n\n"
 mongo_client = Mongo::MongoClient.from_uri(ENV['JOURNAL_MONGO_URI'])
 db = mongo_client.db()
 entries = db.collection('entries')
 
-if ARGV[0] == 'read'
+opts = Trollop::options do
+  version '0.0.1'
+  banner 'sup'
+
+  opt :read
+end
+
+
+say "Preparing journal... \n\n"
+
+if opts.read
   Actions.read entries
 else
   Actions.write entries
